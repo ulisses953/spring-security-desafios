02a43f83cccf2059b9f6ea26d524de434367d5f8
Isaias <isaias@wswork.com.br>
2025-08-07 10:00:53 -0300

    inicio do projeto de bear jwt token

---
+++ jwt_bearer_token/src/main/java/com/jwt_bearer_token/jwt_bearer_token/JwtBearerTokenApplication.java
@@ -0,0 +1,13 @@
+package com.jwt_bearer_token.jwt_bearer_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtBearerTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtBearerTokenApplication.class, args);
+	}
+
+}
---
+++ jwt_bearer_token/src/test/java/com/jwt_bearer_token/jwt_bearer_token/JwtBearerTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwt_bearer_token.jwt_bearer_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtBearerTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
